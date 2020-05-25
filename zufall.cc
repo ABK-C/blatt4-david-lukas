07be497089090ed53438d85d09a9cc3ebdef728d
repl.it user <replituser@example.com>
2020-05-25 16:49:20 +0000

    Blatt4

--- zufall.cc
+++ zufall.cc
@@ -1,2 +1,30 @@
-int main() {
+#include <iostream>
+using namespace std;
+
+
+double zufall(int a, int b, int m)
+{
+  static int I;
+  I=(a*I+b)%m;
+  return double(I)/double(m);
+}
+
+int main()
+{
+  double kreis=0;
+  double quad=100;  
+  for(int i=0; i<quad; ++i)
+  //205,29573,139968
+  //5, 3, 16
+  {
+    double x=zufall(205,29573,139968);
+    double y=zufall(205,29573,139968);
+    if ((x*x+y*y)<1)
+    {
+      ++kreis;
+    }
+  }
+  double pi=kreis/quad*4;
+  cout<< "Pi = "<< pi << "\n";
+  cout << "N_in = " << kreis << "\n";
 }
